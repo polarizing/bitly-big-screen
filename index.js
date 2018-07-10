401aee02ee65bf4b2a2a891096e4a5720742a551
Kevin Li <kli@bit.ly>
2018-07-10 15:45:10 -0400

    added constants

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const expressWs = require('express-ws')(app);
 
+// WEBSOCKET CONSTANTS
+const PLAY_GIF = "PLAY_GIF";
+const CHANGE_STATE = "CHANGE_STATE";
+
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -27,7 +31,7 @@ app.post('/gif', function(req, res) {
     // console.log(req.body);
     var gif_link = req.body.text;
     console.log("Request to play a GIF: " + gif_link);
-    expressWs.getWss().clients.forEach(client => client.send(gif_link));  
+    expressWs.getWss().clients.forEach(client => client.send(PLAY_GIF + " " + gif_link));  
 
     res.json({
         "response_type": "in_channel",
@@ -46,7 +50,7 @@ app.post('/change', function(req, res) {
     console.log("Request to change state of big screen");
     // res.sendStatus(200);
     // ws.send('update');
-    expressWs.getWss().clients.forEach(client => client.send("update"));  
+    expressWs.getWss().clients.forEach(client => client.send(CHANGE_STATE));  
 
     res.json({
     	"response_type": "in_channel",
