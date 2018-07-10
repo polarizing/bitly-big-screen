96e5fae0c730215dba969f14b5fe5999967e017f
Kevin Li <kli@bit.ly>
2018-07-10 09:45:23 -0400

    added sample response json

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post('/test-page', function(req, res) {
     var name = req.body.name,
         color = req.body.color;
     console.log("Got a request!");
-    res.sendStatus(200);
+    // res.sendStatus(200);
     res.json({
     	"response_type": "in_channel",
     	"text": "It's 80 degrees right now.",
