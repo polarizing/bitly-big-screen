033ff2c632a714d30713d6c93078edbb623ffb57
Kevin Li <kli@bit.ly>
2018-07-10 14:31:14 -0400

    added change endpoint

--- index.js
+++ index.js
@@ -22,11 +22,13 @@ app.ws('/', function(ws, req) {
 	console.log('socket', req.testing);
 })
 
-app.post('/test-page', function(req, res) {
-    var name = req.body.name,
-        color = req.body.color;
+app.post('/change', function(req, res) {
+    // var name = req.body.name,
+        // color = req.body.color;
     console.log("Got a request!");
     // res.sendStatus(200);
+    ws.send('update');
+
     res.json({
     	"response_type": "in_channel",
     	"text": "Changing big screen.",
