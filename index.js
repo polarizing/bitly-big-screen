c9ebf302861fe928dd5d802337e4ee7ad962f773
Kevin Li <kli@bit.ly>
2018-07-10 09:44:42 -0400

    added sample response json

--- index.js
+++ index.js
@@ -16,8 +16,18 @@ app.use(express.static('public'))
 app.post('/test-page', function(req, res) {
     var name = req.body.name,
         color = req.body.color;
-    console.log("WHOA");
+    console.log("Got a request!");
     res.sendStatus(200);
+    res.json({
+    	"response_type": "in_channel",
+    	"text": "It's 80 degrees right now.",
+    	"attachments": [
+	        {
+	            "text":"Partly cloudy today and tomorrow"
+	        }
+    	]
+    })
+
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
